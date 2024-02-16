745d8f07a825c861b798eb346e91fe2aa7f54264
svdang999 <101574223+svdang999@users.noreply.github.com>
2024-02-16 11:19:52 +0700

    Create powershell-compress-log-files.ps1

---
+++ powershell-compress-log-files.ps1
@@ -0,0 +1,15 @@
+# Compress files older than xxx days script
+$limit = (Get-Date).AddDays(-30)
+$path = "C:\Test\2015\test\api\Log"
+
+# Get files older than the $limit.
+$folders = Get-ChildItem -Path $path -directory -Force | Where-Object {($_.LastWriteTime -lt $limit)}
+
+# Zip folders into files
+foreach ($folder in $folders) {
+	write-host "start compress folder" $folder
+	Compress-Archive -Path ($folder.FullName + "\*") -DestinationPath ($folder.FullName + ".zip") -CompressionLevel fastest -force
+	# Delete folder after zipped
+	write-host "folder compressed, cleanup folder" $folder
+	Remove-Item -LiteralPath ($folder.FullName) -Force -Recurse
+}
